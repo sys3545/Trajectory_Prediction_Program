19ef4888af2ab33443906837c42d773c049652bb
Sim <61923882+sys3545@users.noreply.github.com>
2021-03-08 18:49:53 +0900

    Update trajectoryMFCDlg.cpp

--- Code/trajectoryMFCDlg.cpp
+++ Code/trajectoryMFCDlg.cpp
@@ -1,41 +1,203 @@
 
-// trajectoryMFCDlg.h: 헤더 파일
+// trajectoryMFCDlg.cpp: 구현 파일
 //
 
-#pragma once
-#include "OPenGLRenderer.h"
+#include "pch.h"
+#include "framework.h"
+#include "trajectoryMFC.h"
+#include "trajectoryMFCDlg.h"
+#include "afxdialogex.h"
 
-// CtrajectoryMFCDlg 대화 상자
-class CtrajectoryMFCDlg : public CDialogEx
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
 {
-	OPenGLRenderer* m_test; // OpenGL 객체
-// 생성입니다.
 public:
-	CtrajectoryMFCDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	CAboutDlg();
 
 // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_TRAJECTORYMFC_DIALOG };
+	enum { IDD = IDD_ABOUTBOX };
 #endif
 
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
-
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
 
 // 구현입니다.
 protected:
-	HICON m_hIcon;
-
-	// 생성된 메시지 맵 함수
-	virtual BOOL OnInitDialog();
-	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
-	afx_msg void OnPaint();
-	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
-public:
-	CStatic m_pLeft; // picture 객체 변수
-	CRect rectLeft; // picture 사각형 변수
-	int xpos, ypos, zpos; // 입력받을 포지션의 변수
-	afx_msg void OnTimer(UINT_PTR nIDEvent); // WM_TIMER로 추가한 함수
-	afx_msg void OnBnClickedButtonAdd(); // ADD 버튼이 클릭되면
 };
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CtrajectoryMFCDlg 대화 상자
+
+
+
+CtrajectoryMFCDlg::CtrajectoryMFCDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_TRAJECTORYMFC_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CtrajectoryMFCDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_STATIC_PICTURE, m_pLeft);
+}
+
+BEGIN_MESSAGE_MAP(CtrajectoryMFCDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_TIMER()
+	ON_BN_CLICKED(IDC_BUTTON_ADD, &CtrajectoryMFCDlg::OnBnClickedButtonAdd)
+END_MESSAGE_MAP()
+
+
+// CtrajectoryMFCDlg 메시지 처리기
+
+BOOL CtrajectoryMFCDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	SetWindowText(_T("Trajectory Prediction of Two-Body Problem - SHIM EUN SONG")); // Window title 설정
+	SetBackgroundColor(RGB(255, 255, 255)); // Window 배경색 설정
+	SetTimer(1000, 10, NULL);
+
+	// OpenGL 생성 및 초기화 작업
+	//CRect rectLeft;
+	int iWidth, iHeight;
+
+	m_pLeft.GetWindowRect(rectLeft);
+	iWidth = rectLeft.Width();
+	iHeight = rectLeft.Height();
+
+	m_test = new OPenGLRenderer;
+	CString className = AfxRegisterWndClass(CS_HREDRAW | CS_VREDRAW | CS_OWNDC, NULL, (HBRUSH)GetStockObject(WHITE_BRUSH), NULL);
+	m_test->CreateEx(0, className, _T("OpenGLCube"), WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN, rectLeft, this, 0);
+	m_test->ShowWindow(SW_SHOW);
+
+	m_test->CreateGLContext(rectLeft, this);
+	m_test->PrepareScene(0, 0, iWidth, iHeight); //will show without this but as black & white.
+	m_test->SetTimer(1, 10, 0);
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CtrajectoryMFCDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CtrajectoryMFCDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+		
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CtrajectoryMFCDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CtrajectoryMFCDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	SetDlgItemText(IDC_EDIT1, m_test->test);
+	CDialogEx::OnTimer(nIDEvent);
+}
+
+
+void CtrajectoryMFCDlg::OnBnClickedButtonAdd() // ADD 버튼이 클릭되면
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	xpos = GetDlgItemInt(IDC_EDIT_XPOS); // XPOS 텍스트박스에 적힌 값을 xpos에 저장 
+	ypos = GetDlgItemInt(IDC_EDIT_YPOS);
+	zpos = GetDlgItemInt(IDC_EDIT_ZPOS);
+	
+	m_test->spaceCraft[m_test->numOfCraft].xpos = (GLfloat)xpos; // 포지션값을 해당하는 물체의 정보에 저장
+	m_test->spaceCraft[m_test->numOfCraft].ypos = (GLfloat)ypos;
+	m_test->spaceCraft[m_test->numOfCraft].zpos = (GLfloat)zpos;
+
+	m_test->CreateCraft(m_test->numOfCraft); // 우주물체 생성
+
+	m_test->numOfCraft++; // 개체수 증가
+}
