2886f73b5bc2ef37b1ebf9ed861e3520a1fe636b
Sim <61923882+sys3545@users.noreply.github.com>
2021-03-09 19:15:11 +0900

    Update OPenGLRenderer.h

--- Code/OPenGLRenderer.h
+++ Code/OPenGLRenderer.h
@@ -9,11 +9,16 @@
 #pragma comment(lib,"legacy_stdio_definitions.lib")
 
 typedef struct {
-	GLfloat xpos = 0.0f;
-	GLfloat ypos = 0.0f;
-	GLfloat zpos = 0.0f;
+	GLfloat xpos = 0.0f, ypos = 0.0f, zpos = 0.0f;
+	GLfloat xvel = 0.0f, yvel = 0.0f, zvel = 0.0f;
+	GLfloat C1 = 0.0f, C2 = 0.0f, C3 = 0.0f;
+	GLfloat h = 0.0f;
+	GLfloat angleSpeed = 36.0f; // angle per t
+	GLfloat angle = 0.0f;
+	GLfloat radius = 7.0f;
 	GLUquadricObj* craft = NULL;
 }vecInfo_t;
+
 
 class OPenGLRenderer : public CWnd
 {
@@ -30,7 +35,6 @@ public:
 	// 텍스처 관련 함수
 	AUX_RGBImageRec* LoadBMPFile(char* filename); // 변수에 이미지를 불러오는 함수
 	int LoadGLTextures(); //텍스쳐 작업을 해주는 함수
-	CString test=(CString)"STEP0";
 public:
 	// 지구 회전 성분
 	GLfloat		xrot = 0;
@@ -51,6 +55,14 @@ public:
 
 	vecInfo_t spaceCraft[5]; // 우주물체 객체
 	int numOfCraft = 0; // 우주물체 갯수
+
+	/// 시간관련 변수
+	GLfloat time = 0.000278f; // 단위시간 지정 (초당 속도를 나타내기 위해) -> 0.000278을 기준으로 초당 각속도를 나타낼 수 있음
+	int currentTime = 0;
+	int oldTime = 0;
+	int deltaTime = 0;
+
+	CString test;
 
 	// 텍스처 관련 변수
 	AUX_RGBImageRec* pTextureImage[1]; // 이미지 파일에 대한 정보를 가지고 있는 배열
