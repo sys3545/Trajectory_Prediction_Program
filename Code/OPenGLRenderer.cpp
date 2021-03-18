c11865dfb8c2e2d1a457c915cd52526243da0565
Shim <61923882+sys3545@users.noreply.github.com>
2021-03-18 21:26:38 +0900

    Update OPenGLRenderer.cpp

--- Code/OPenGLRenderer.cpp
+++ Code/OPenGLRenderer.cpp
@@ -4,6 +4,9 @@
 
 BEGIN_MESSAGE_MAP(OPenGLRenderer, CWnd)
 	ON_WM_TIMER()
+	ON_WM_KEYDOWN()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_MOUSEMOVE()
 END_MESSAGE_MAP()
 
 
@@ -120,7 +123,7 @@ void OPenGLRenderer::PrepareScene(int sx, int sy, int cx, int cy)
 
 	glEnable(GL_DEPTH_TEST); // 깊이 버퍼를 사용할 것을 알림
 
-	
+
 	earth = gluNewQuadric(); // 지구 객체 인스턴스 생성
 	moon = gluNewQuadric(); // 달 객체 인스턴스 생성
 	spaceCraft[4].craft = gluNewQuadric();
@@ -140,7 +143,7 @@ bool OPenGLRenderer::initAi()
 	glEnable(GL_LIGHT1);
 
 	wglMakeCurrent(m_hdc, NULL);
-	
+
 
 	return true;
 }
@@ -156,7 +159,7 @@ int OPenGLRenderer::DrawGLScene()
 	glPushMatrix(); // 지구중심좌표 추가
 	glTranslatef(0.0f, 0.0f, -30.0f); //원점의 이동
 	glRotatef(-90.0f, 1.0f, 0.0f, 0.0f); // x축을 기준으로 좌표축 -90도 회전( 자전축 맞추기용 )
-	//glRotatef(-23.5f, 0.0f, 1.0f, 0.0f); // y축을 기준으로 좌표축 -23.5도 회전( 자전축 맞추기용 )
+	//glRotatef(zAngle, 0.0f, 0.0f, 1.0f); // z축을 기준으로 좌표축 회전(화면 회전용)
 
 	glRotatef(zrot, 0.0f, 0.0f, 1.0f); //회전 변환 (z축) (지구 자전)
 	gluQuadricDrawStyle(earth, GLU_FILL); // 객체를 채우는 형태로 설정
@@ -182,7 +185,7 @@ int OPenGLRenderer::DrawGLScene()
 	glRotatef(moon_zrot, 0.0f, 1.0f, 0.0f); //회전 변환 (z축) (달 공전) , 그리고 회전을 적용
 	glTranslatef(7.0f, 0.0f, 0.0f); // 지구와의 거리만큼 x축에서 이동
 	glRotatef(-90.0f, 1.0f, 0.0f, 0.0f); // x축을 기준으로 좌표축 -90도 회전( 회전축 맞추기용 )
-	
+
 	gluQuadricDrawStyle(moon, GLU_FILL); // 객체를 채우는 형태로 설정
 	glColor3f(0.35f, 0.35f, 0.35f);
 	gluSphere(moon, 0.3f, 12, 12);
@@ -211,7 +214,7 @@ int OPenGLRenderer::DrawGLScene()
 			glPopMatrix();
 
 			spaceCraft[n].angle += spaceCraft[n].angleSpeed * time; // 각도 증가
-			if (spaceCraft[n].angle > 359.9f) spaceCraft[n].angle = 0.0f; // 360도 넘으면 0도로 
+			if (spaceCraft[n].angle > 359.9f) spaceCraft[n].angle = 0.0f; // 360도 넘으면 0도로
 
 			spaceCraft[n].xpos = spaceCraft[n].radius * (GLfloat)cos(spaceCraft[n].angle);
 			spaceCraft[n].ypos = spaceCraft[n].radius * (GLfloat)sin(spaceCraft[n].angle);
@@ -269,7 +272,7 @@ int OPenGLRenderer::DrawGLScene()
 
 	glFlush();
 
-	zrot += 0.5f; 
+	zrot += 0.5f;
 	if (zrot > 359.5f) zrot = 0.0f;
 
 	moon_zrot += 1.0f;
@@ -279,7 +282,7 @@ int OPenGLRenderer::DrawGLScene()
 		deltaTime2 = currentTime2 - oldTime2;
 		oldTime2 = currentTime2; /// 달의 주기 확인 결과( 1 tick = 0.018333 s )
 	}
-	
+
 	return TRUE;
 }
 
@@ -300,16 +303,16 @@ AUX_RGBImageRec* OPenGLRenderer::LoadBMPFile(char* filename)
 {
 	FILE* hFile = NULL;
 	if (!filename) return NULL;
-	
+
 	hFile = fopen(filename, "r");
 	wchar_t wtext[20];
 	mbstowcs(wtext, filename, strlen(filename) + 1);//Plus null
 	LPWSTR ptr = wtext;
-	
+
 	if (hFile) {
 		fclose(hFile);
 		return auxDIBImageLoad(ptr);
-	} 
+	}
 
 	return NULL;
 }
@@ -319,7 +322,7 @@ int OPenGLRenderer::LoadGLTextures()
 	int Status = FALSE;
 
 	gluQuadricTexture(earth, GL_TRUE); // 텍스처 매핑 사용
-	memset(pTextureImage, 0, sizeof(void *) * 1); // 포인터 초기화
+	memset(pTextureImage, 0, sizeof(void*) * 1); // 포인터 초기화
 
 	if (pTextureImage[0] = LoadBMPFile("earthbmp.bmp")) {
 		Status = TRUE;
@@ -328,7 +331,7 @@ int OPenGLRenderer::LoadGLTextures()
 		glTexImage2D(GL_TEXTURE_2D, 0, 3, pTextureImage[0]->sizeX, pTextureImage[0]->sizeY, 0,
 			GL_RGB, GL_UNSIGNED_BYTE, pTextureImage[0]->data);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // 이미지 파일과 물체의 크기를 맞춰준다
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); 
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glEnable(GL_TEXTURE_2D);
 	}
 
@@ -365,5 +368,36 @@ void OPenGLRenderer::DrawTrajectory(int num) {
 
 // 우주물체 구를 그려주는 함수
 void OPenGLRenderer::DrawSphere(int num) {
+	glRotatef(spaceCraft[num].w, 0.0f, 0.0f, 1.0f); // w 만큼 그리는 기준면 회전
+	// 여기서 f만큼 회전한 위치에서 초기 구체 생성
+	spaceCraft[num].xpos = (GLfloat)cos(spaceCraft[num].f * GL_PI / 180.0f) * spaceCraft[num].range / 1000.0f;
+	spaceCraft[num].ypos = (GLfloat)sin(spaceCraft[num].f * GL_PI / 180.0f) * spaceCraft[num].range / 1000.0f;
 
+	glTranslatef(spaceCraft[num].xpos, spaceCraft[num].ypos, 0.0f); // 구체 이동 (방정식에 따라 이 포지션이 바뀐다)
+	gluQuadricDrawStyle(spaceCraft[num].craft, GLU_FILL); // 객체를 채우는 형태로 설정
+	glColor3f(0.35f, 0.35f, 0.35f);
+	gluSphere(spaceCraft[num].craft, 0.3f, 12, 12);
+
+	gluQuadricDrawStyle(spaceCraft[num].craft, GLU_LINE); // 선을 긋는 형태로 설정
+	glColor3f(0.7f, 0.7f, 0.7f);
+	gluSphere(spaceCraft[num].craft, 0.32f, 12, 12);
+}
+
+
+void OPenGLRenderer::OnLButtonDown(UINT nFlags, CPoint point) // 클릭하면 클릭 시의 마우스 위치가 저장된다.
+{
+	GLfloat x;
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	x = point.x;
+	mousePoint = x;
+	CWnd::OnLButtonDown(nFlags, point);
+}
+
+
+void OPenGLRenderer::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	CWnd::OnMouseMove(nFlags, point);
 }
